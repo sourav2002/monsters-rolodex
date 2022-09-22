a814d4e5ef820cc7ccdff86c58d15ea48edc37ab
sourav2002 <sainisourav233@gmail.com>
2022-09-22 21:07:28 +0530

    initial commit

---
+++ src/components/search-bar/search-bar.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  './search-bar.styles.css'
+
+export const SearchBar = ({placeholder, handleChange}) =>{
+    return <input
+    className="search-bar" 
+    type='search' 
+    placeholder={placeholder}  
+    onChange={handleChange} />
+}
